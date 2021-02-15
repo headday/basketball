cf57137b1ad6fbcabf8c7f04e8cf3db459f40cbb
kirill <svezhinovkirill17@gmail.com>
2021-02-15 13:12:24 +0500

    delete uselles service

--- src/service/service.js
+++ src/service/service.js
@@ -7,12 +7,4 @@ const getData = async (url) =>{
     
     return await res.json();
 }
-const getStats = async (url) =>{
-    const res = await (fetch(url))
-    if(!res.ok){
-        alert('fetch error')
-    }
-    
-    return await res.json();
-}
-export {getData,getStats}
+export {getData}
