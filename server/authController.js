47f1673a4a8cf30443ea4f283836563d1ab88b1c
kirill <svezhinovkirill17@gmail.com>
2021-02-28 14:39:52 +0500

    add controller

--- server/authController.js
+++ server/authController.js
@@ -1,11 +1,27 @@
 class authController{
-    async registration(){
+    async registration(req,res){
+        try {
+            
+        } catch (e) {
+            
+        }
 
     }
-    async login(){
+    async login(req,res){
+        try {
+            
+        } catch (e) {
+            
+        }
         
     }
-    async getUsers(){
+    async getUsers(req,res){
+        try {
+            res.json("server work")
+        } catch (e) {
+            
+        }
         
     }
 }
+module.exports = new authController
--- server/main.js
+++ server/main.js
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(express.json())
 app.use('/auth',authRouter)
 
-
-app.get('/test', (req, res) => {
-  res.json({msg:'test'})
-})
 const start = async () =>{
   try {
     await mongoose.connect(mongoProfile)
