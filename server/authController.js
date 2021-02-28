96d060b8ec279a4b10a539b8b67e6ef095623cbb
kirill <svezhinovkirill17@gmail.com>
2021-02-28 15:37:15 +0500

    add middleware

--- server/authController.js
+++ server/authController.js
@@ -1,13 +1,18 @@
 const User = require('./models/User')
 const bcrypt = require('bcrypt');
+const {validationResult} = require('express-validator')
 class authController{
     async registration(req,res){
         try {
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({message:"Registration error",errors})
+            }
             const {username,password} = req.body
             const condidate = await User.findOne({username})
             if(condidate){
                  res.status(400).json({message:"User is already created"})
-            }
+            }                                                                                                      
             const hashPassword = bcrypt.hashSync(password, 7);
             const user = new User({username,password:hashPassword})
             await user.save();
--- server/authRouter.js
+++ server/authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const router = new Router();
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration',controller.registration)
+router.post('/registration',[
+    check('username', "user name is not be empty").notEmpty(),
+    check('password',"Password must be longer than 10 characters").isLength({min:10,max:20})
+],controller.registration)
 router.post('/login',controller.login)
 router.get('/users',controller.getUsers)
 
