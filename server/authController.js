03d4d11bbb41da69a6bd358833ce5b2c9374ab6f
kirill <svezhinovkirill17@gmail.com>
2021-02-28 14:21:39 +0500

    mongo connection

---
+++ server/authController.js
@@ -0,0 +1,11 @@
+class authController{
+    async registration(){
+
+    }
+    async login(){
+        
+    }
+    async getUsers(){
+        
+    }
+}
--- server/main.js
+++ server/main.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const cors = require('cors')
-
+const mongoose = require("mongoose");
 const app = express()
-const port = 4000
+const PORT = 4000
+const mongoProfile = require('./config')
+const authRouter = require('./authRouter')
 
 app.use(cors());
+app.use(express.json())
+app.use('/auth',authRouter)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/test', (req, res) => {
+  res.json({msg:'test'})
 })
+const start = async () =>{
+  try {
+    await mongoose.connect(mongoProfile)
+    app.listen(PORT, () => {
+      console.log(`Server app listening at http://localhost:${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+start();
