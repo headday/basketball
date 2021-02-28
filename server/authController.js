04bef7650e8e078e7afb853ce5e9cd091eb54603
kirill <svezhinovkirill17@gmail.com>
2021-02-28 15:01:50 +0500

    add registration controller

--- server/authController.js
+++ server/authController.js
@@ -1,9 +1,20 @@
+const User = require('./models/User')
+const bcrypt = require('bcrypt');
 class authController{
     async registration(req,res){
         try {
-            
+            const {username,password} = req.body
+            const condidate = await User.findOne({username})
+            if(condidate){
+                 res.status(400).json({message:"User is already created"})
+            }
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const user = new User({username,password:hashPassword})
+            await user.save();
+            return res.json({message:"User created"})
         } catch (e) {
-            
+            console.log(e)
+            res.status(400).json({message:"Registration error"})
         }
 
     }
@@ -11,7 +22,8 @@ class authController{
         try {
             
         } catch (e) {
-            
+            console.log(e)
+            res.status(400).json({message:"Login error"})
         }
         
     }
